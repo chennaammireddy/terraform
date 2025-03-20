120c1ae428d891988070c909e62f11a50626625a
chenna <chennaammireddy@gmail.com>
2025-03-20 21:10:44 +0530

    first_terraform

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,8 +1,21 @@
-provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+# Terraform Settings Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      #version = "~> 3.21" # Optional but recommended in production
+    }
+  }
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-08b5b3a93ed654d19"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+# Provider Block
+provider "aws" {
+  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  region  = "us-east-1"
+}
+
+# Resource Block
+resource "aws_instance" "ec2demo" {
+  ami           = "ami-08b5b3a93ed654d19" # Amazon Linux in us-east-1, update as per your region
+  instance_type = "t2.micro"
 }
